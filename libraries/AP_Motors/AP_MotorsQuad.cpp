b6ead80f43551c2985b31bfa4ef3bcd40ae74efa
priseborough <p_riseborough@live.com.au>
2017-03-07 21:45:51 +1100

    AP_Motors: Custom motor mix for dual wind motors

--- libraries/AP_Motors/AP_MotorsQuad.cpp
+++ libraries/AP_Motors/AP_MotorsQuad.cpp
@@ -44,11 +44,14 @@ void AP_MotorsQuad::setup_motors()
 
     }else if( _flags.frame_orientation == AP_MOTORS_H_FRAME ) {
         // H frame set-up - same as X but motors spin in opposite directiSons
-        // NOT FOR MASTER - Narrow frame customisation - NOT FOR MASTER
-        add_motor(AP_MOTORS_MOT_1,   17, AP_MOTORS_MATRIX_YAW_FACTOR_CW,  1);
-        add_motor(AP_MOTORS_MOT_2, -163, AP_MOTORS_MATRIX_YAW_FACTOR_CW,  3);
-        add_motor(AP_MOTORS_MOT_3,  -17, AP_MOTORS_MATRIX_YAW_FACTOR_CCW, 4);
-        add_motor(AP_MOTORS_MOT_4,  163, AP_MOTORS_MATRIX_YAW_FACTOR_CCW, 2);
+        add_motor(AP_MOTORS_MOT_1,   45, AP_MOTORS_MATRIX_YAW_FACTOR_CW,  1);
+        add_motor(AP_MOTORS_MOT_2, -135, AP_MOTORS_MATRIX_YAW_FACTOR_CW,  3);
+        add_motor(AP_MOTORS_MOT_3,  -45, AP_MOTORS_MATRIX_YAW_FACTOR_CCW, 4);
+        add_motor(AP_MOTORS_MOT_4,  135, AP_MOTORS_MATRIX_YAW_FACTOR_CCW, 2);
+        add_motor(AP_MOTORS_MOT_5,   45, AP_MOTORS_MATRIX_YAW_FACTOR_CW,  1);
+        add_motor(AP_MOTORS_MOT_6, -135, AP_MOTORS_MATRIX_YAW_FACTOR_CW,  3);
+        add_motor(AP_MOTORS_MOT_7,  -45, AP_MOTORS_MATRIX_YAW_FACTOR_CCW, 4);
+        add_motor(AP_MOTORS_MOT_8,  135, AP_MOTORS_MATRIX_YAW_FACTOR_CCW, 2);
     }else if(_flags.frame_orientation == AP_MOTORS_VTAIL_FRAME) {
         /*
             Tested with: Lynxmotion Hunter Vtail 400
