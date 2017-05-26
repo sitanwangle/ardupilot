af4ac507d84434d320e2c8f890b8a6d9d88f5ac6
priseborough <p_riseborough@live.com.au>
2017-05-27 09:28:39 +1000

    AP_NavEKF2: Add sample interval to optical flow interface
    Enables improvements to handling of timing jitter

--- libraries/AP_NavEKF2/AP_NavEKF2.h
+++ libraries/AP_NavEKF2/AP_NavEKF2.h
@@ -203,7 +203,8 @@ public:
     // The sign convention is that a RH physical rotation of the sensor about an axis produces both a positive flow and gyro rate
     // msecFlowMeas is the scheduler time in msec when the optical flow data was received from the sensor.
     // posOffset is the XYZ flow sensor position in the body frame in m
-    void  writeOptFlowMeas(uint8_t &rawFlowQuality, Vector2f &rawFlowRates, Vector2f &rawGyroRates, uint32_t &msecFlowMeas, const Vector3f &posOffset);
+    // deltaTime is the time in seconds that the flow data was accumulated across
+    void  writeOptFlowMeas(uint8_t &rawFlowQuality, Vector2f &rawFlowRates, Vector2f &rawGyroRates, uint32_t &msecFlowMeas, const Vector3f &posOffset, float &deltaTime);
 
     // return data for debugging optical flow fusion for the specified instance
     // An out of range instance (eg -1) returns data for the the primary instance
