ee6cc732f9ffd1108360b3e187ba586af68acb1c
priseborough <p_riseborough@live.com.au>
2017-10-04 17:11:28 +1100

    AP_NavEKF3: Add debug interface for ext nav scale factor estimator

--- libraries/AP_NavEKF3/AP_NavEKF3.h
+++ libraries/AP_NavEKF3/AP_NavEKF3.h
@@ -286,6 +286,8 @@ public:
     bool getRangeBeaconDebug(int8_t instance, uint8_t &ID, float &rng, float &innov, float &innovVar, float &testRatio, Vector3f &beaconPosNED,
                              float &offsetHigh, float &offsetLow, Vector3f &posNED);
 
+    bool getScaleFactorDebug(int8_t instance, float &scaleLog, Vector3f &innov, Vector3f &innovVar);
+
     // called by vehicle code to specify that a takeoff is happening
     // causes the EKF to compensate for expected barometer errors due to ground effect
     void setTakeoffExpected(bool val);
